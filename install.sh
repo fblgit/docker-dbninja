97d9276905652ccdaf8caa3088e70090d6ef7e60
FBLGit <fblgit@users.noreply.github.com>
2016-04-22 11:27:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 chmod 755 /data
 cd /data
 wget https://www.dbninja.com/download/dbninja.tar.gz
-mkdir /data/www
-rm -rf /var/www
-ln /data/www /var/www -s
+mkdir /data/www && cd /data/www && rm -rf /var/www && ln /data/www /var/www -s
+tar zxf /data/dbninja.tar.gz
 chown -R www-data:www-data /data/www
 sed -i -e"s/#LogLevel info ssl\:warn/Options \+FollowSymLinks/" /etc/apache2/sites-enabled/000-default.conf
-sed -i -e"s/DocumentRoot \/var\/www/DocumentRoot \/var\/www\/dbninja/" /etc/apache2/sites-enabled/000-default.conf
+sed -i -e"s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/dbninja/" /etc/apache2/sites-enabled/000-default.conf
 sed -i -e"s/upload_max_filesize \= 2M/upload_max_filesize \= 1024M/" /etc/php5/apache2/php.ini
